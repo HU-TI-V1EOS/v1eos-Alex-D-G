02f2c18db44a07790e52ed298a8326b469c65b57
Alex-D-G <alexander.de.graaf@casema.nl>
2020-03-24 13:50:38 +0100

    Added info

--- os_level/tool.cc
+++ os_level/tool.cc
@@ -19,14 +19,16 @@ std::string translate(std::string line, std::string argument){
 
 int main(int argc, char *argv[]){
 	std::string line;
-
+	std::cout << "Geef een aantal karakters/woorden mee die je uit de meegegeven string wilt halen" << std::endl;
+	std::cout << "-----------" << std::endl;
 	if(argc != 2){
 		std::cerr << "Deze functie heeft exact 1 argument nodig" << std::endl;
 		return -1;
 	}
-
-	while(std::getline(std::cin, line)){
+	while(std::cout << "Verwijder:", std::getline(std::cin, line)){
+		std::cout << "Output:";
 		std::cout << translate(line, argv[1]) << std::endl;
+		std::cout << "-----------" << std::endl;
 	}
 
   	return 0;
